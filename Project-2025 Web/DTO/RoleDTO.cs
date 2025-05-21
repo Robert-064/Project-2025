5571549e40828479dcedf829397f6d14b9b68a22
Pablo Sierra <juansierra1116731@correo.itm.edu.co>
2025-05-21 12:02:18 -0500

    role CRUD

---
+++ Project-2025 Web/DTO/RoleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_2025_Web.DTO
+{
+    public class RoleDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
+        public string Name { get; set; }
+    }
+}
